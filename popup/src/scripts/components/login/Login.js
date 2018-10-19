76a1ac8348c0c55af61ee9ae80a908b107a8056d
udithv <udithv01@gmail.com>
2018-10-19 11:08:30 +0530

    added new login banner

--- popup/src/scripts/components/login/Login.js
+++ popup/src/scripts/components/login/Login.js
@@ -1,12 +1,4 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-
-import { Store } from 'react-chrome-redux';
-
-
-const proxyStore = new Store({
-  portName: 'errordock'
-});
 
 class Login extends Component {
   constructor(props) {
@@ -17,7 +9,7 @@ class Login extends Component {
     return (
         <div className="login">
             <div className="login__logo-box">
-                <img className="login__logo" src="img/login_banner.svg" alt="svg image" />
+                <img className="login__logo" src="img/loginbanner.svg" alt="svg image" />
             </div>
             <div className="u-margin-bottom-small">
                 <a 
@@ -57,23 +49,6 @@ class Login extends Component {
       </div>
     )
   }
-
-/*   render() {
-    return(   
-              <div>
-                <button 
-                  onClick={() =>  window.open("http://localhost:5000/auth/google")}
-                >
-                  Login
-                </button>
-                <button 
-                  onClick={() => proxyStore.dispatch({ type:'DOCK_IT'})}
-                >
-                  say hi
-                </button>
-              </div>
-            )
-            } */
 
 }
 
